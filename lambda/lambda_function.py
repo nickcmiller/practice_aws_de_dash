1489ce410e421ff9c51f1f7d2e4307f440ed8a1d
Nick Miller <miller.nick.c@gmail.com>
2025-02-03 16:47:42 +0000

    Update function

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -7,19 +7,20 @@ def lambda_handler(event, context):
     first_of_this_month = today.replace(day=1)
     last_month_end = first_of_this_month - datetime.timedelta(days=1)
     last_month_start = last_month_end.replace(day=1)
+    time_period = {
+                'Start': last_month_start.strftime('%Y-%m-%d'),
+                'End': (last_month_end + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+            }
     
     ce = boto3.client('ce')
     
     try:
         response = ce.get_cost_and_usage(
-            TimePeriod={
-                'Start': last_month_start.strftime('%Y-%m-%d'),
-                'End': (last_month_end + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
-            },
-            Granularity='MONTHLY',
+            TimePeriod=time_period,
+            Granularity='DAILY',
             Metrics=['UnblendedCost']
         )
-        cost_data = response['ResultsByTime'][0]['Total']['UnblendedCost']['Amount']
+        cost_data = response['ResultsByTime'][0]['Groups'][0]['Metrics']
         message = f"Cost for {last_month_start.strftime('%B %Y')}: ${cost_data}"
     except Exception as e:
         message = f"Error retrieving cost data: {str(e)}"
