b60824c445ec1cb92bccc4a384da868f230534c4
Nick Miller <miller.nick.c@gmail.com>
2025-02-04 21:54:37 +0000

    add react folder

---
+++ react/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the React Static Website</h1>
+      <p>This website is hosted on S3 using Terraform.</p>
+    </div>
+  );
+}
+
+export default App;
